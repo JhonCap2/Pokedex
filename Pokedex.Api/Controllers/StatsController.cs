ba565b91a7bf8b6bf639340824597b9dd9db79ec
Jhon Adames <jhon.adames@ceo.do>
2023-05-20 10:19:51 -0400

    Controller actualizados

---
+++ Pokedex.Api/Controllers/StatsController.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Core.DTOs;
+using Pokedex.Core.Entities;
+using Pokedex.Core.Interface;
+
+namespace Pokedex.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatsController : Controller
+    {
+        private IStatsRepository _statsRepository;
+        private readonly IMapper _mapper;
+
+        public StatsController(IStatsRepository statsRepository, IMapper mapper)
+        {
+            _statsRepository = statsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            try
+            {
+                var stats = await _statsRepository.All();
+                var statsDto = _mapper.Map<IEnumerable<StatsDto>>(stats);
+                return Ok(statsDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var stat = await _statsRepository.Get(id);
+                var statDto = _mapper.Map<StatsDto>(stat);
+                return Ok(statDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(StatsDto newStats)
+        {
+            try
+            {
+                var stats = _mapper.Map<Stats>(newStats);
+                await _statsRepository.Insert(stats);
+                return Ok(stats);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(StatsDto updatestats)
+        {
+            try
+            {
+                var stats = _mapper.Map<Stats>(updatestats);
+                await _statsRepository.Update(stats);
+                return Ok(stats);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var stats = await _statsRepository.Get(id);
+                var deleteStats = await _statsRepository.Delete(id);
+                if (deleteStats)
+                    return Ok($"Los stats del pokemon fueron eliminado correctamente");
+                else
+                    return StatusCode(500, $"Operación no procesada o el GUID '{id}' no existe.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Pokedex.Api/Controllers/TypeController.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Core.DTOs;
+using Pokedex.Core.Entities;
+using Pokedex.Core.Interface;
+
+namespace Pokedex.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeController : Controller
+    {
+        private ITypesRepository _typesRepository;
+        private readonly IMapper _mapper;
+
+        public TypeController(ITypesRepository typesRepository, IMapper mapper)
+        {
+            _typesRepository = typesRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            try
+            {
+                var types = await _typesRepository.All();
+                var typesDto = _mapper.Map<IEnumerable<Types>>(types);
+                return Ok(typesDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var type = await _typesRepository.Get(id);
+                var typeDto = _mapper.Map<TypeDto>(type);
+                return Ok(typeDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(TypeDto newType)
+        {
+            try
+            {
+                var types = _mapper.Map<Types>(newType);
+                await _typesRepository.Insert(types);
+                return Ok(types);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(TypeDto updateType)
+        {
+            try
+            {
+                var types = _mapper.Map<Types>(updateType);
+                await _typesRepository.Update(types);
+                return Ok(types);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var types = await _typesRepository.Get(id);
+                var deletetypes = await _typesRepository.Delete(id);
+                if (deletetypes)
+                    return Ok($"El tipo {types.Name} fue eliminado correctamente");
+                else
+                    return StatusCode(500, $"Operación no procesada o el GUID '{id}' no existe.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
